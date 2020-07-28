ff2a7c158e862211a0c501778f3587a1170808b4
ludo77100 <ludovic.s@protonmail.com>
2020-07-28 14:48:15 +0200

    ajout du test getTotalDebit dans EcritureComptableTest

--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -40,5 +40,23 @@ public class EcritureComptableTest {
         vEcriture.getListLigneEcriture().add(this.createLigne(2, "1", "2"));
         Assert.assertFalse(vEcriture.toString(), vEcriture.isEquilibree());
     }
+    
+    @Test
+    public void getTotalDebit(){
+
+        EcritureComptable vEcriture;
+        vEcriture = new EcritureComptable();
+
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, "200.50", null));
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, "100.50", "33"));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "301"));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, "40", "7"));
+
+        Assert.assertEquals(vEcriture.getTotalCredit(), BigDecimal.valueOf(33+301+7));
+    }
+
+    @Test
+    public void getTotalCredit() {
+    }
 
 }
